2cce89f0d6ad0c05a0214dbf784b15a437c76dee
Toan Nguyen <toan.nguyen@smartdev.com>
2022-08-01 21:34:30 +0700

    fix conflict c4,c5

--- 1.js
+++ 1.js
@@ -1,3 +1,6 @@
 // day la dong code so 1
 // dong code thu 2, sua tren master
 // dong code thu 3, sua tren master
+// dong code thu 2, sua tren master,
+// fix conflict when rebase from master
+// fix conflict tiep nua ne
